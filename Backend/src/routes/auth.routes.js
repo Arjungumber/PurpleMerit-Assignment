288a7cc5839453bf76b6833cd51bc0e0958c30af
Arjungumber <gumberarjun07@gmail.com>
2025-12-30 22:23:58 +0530

    vercel error fixed

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { signup, login, getMe } = require("../controllers/auth.controller");
-const authMiddleware = require("../middleware/auth.middleware");
+const authMiddleware = require("../middleware/auth.Middleware.js");
 
 const router = express.Router();
 
